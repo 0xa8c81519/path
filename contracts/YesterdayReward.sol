02ba93610c98521ebc5eb860c4fd440f06dfc782
0xa8c81519 <0xa8c81519@gmail.com>
2021-06-28 15:01:47 +0800

    trade mining

---
+++ contracts/YesterdayReward.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./lib/TransferHelper.sol";
+
+contract YesterdayReward {
+    using SafeMath for uint256;
+
+    // todo: Implement Airdrop
+
+    IERC20 public token;
+
+    constructor() {}
+}
